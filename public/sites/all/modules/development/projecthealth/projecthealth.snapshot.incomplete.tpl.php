91b2b5823611c7e89997226aa354b5ad27fc80b8
D34dMan <shibinkidd@gmail.com>
2016-02-11 16:29:19 +0100

    Snapshot incomplete template uses proper variable.

--- public/sites/all/modules/development/projecthealth/projecthealth.snapshot.incomplete.tpl.php
+++ public/sites/all/modules/development/projecthealth/projecthealth.snapshot.incomplete.tpl.php
@@ -5,12 +5,12 @@
         <span class="info-box-icon"><i class="fa fa-info"></i></span>
         <div class="info-box-content">
           <span class="info-box-text">Index status</span>
-          <span class="info-box-number"><?php echo $total['snapshot']->progress; ?>%</span>
+          <span class="info-box-number"><?php echo $snapshot->progress; ?>%</span>
           <div class="progress">
-            <div class="progress-bar" style="width: <?php echo $total['snapshot']->progress; ?>%"></div>
+            <div class="progress-bar" style="width: <?php echo $snapshot->progress; ?>%"></div>
           </div>
           <span class="progress-description">
-            <?php echo format_date($total['snapshot']->created); ?>
+            <?php echo format_date($snapshot->created); ?>
           </span>
         </div>
       </div>
