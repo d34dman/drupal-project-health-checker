4e153028024acdc54847c300e0f28ddf5d1184b0
D34dMan <shibinkidd@gmail.com>
2016-03-19 13:28:13 +0100

    Placed a footnote on incomplete snapshot page.

--- public/sites/all/modules/development/projecthealth/projecthealth.snapshot.incomplete.tpl.php
+++ public/sites/all/modules/development/projecthealth/projecthealth.snapshot.incomplete.tpl.php
@@ -16,4 +16,30 @@
       </div>
     </div>
   </div>
+
+  <div class="row">
+    <div class="col-lg-12 col-xs-12">
+      <!-- small box -->
+      <div class="small-box bg-gray">
+        <div class="inner">
+          <h3 class="title"></h3>
+          <p class="message-default">
+            Snapshot have been queued for creation. The data for snapshot is retrived from Drupal.org
+            using its REST API on cron runs. If there is a huge number of user activity on the project then the
+            snapshot creation might take days (for example Drupal Core project takes about 3 days to complete).
+            <br/>
+          </p>
+          <p class="message-dynamic">
+          </p>
+        </div>
+        <div class="icon">
+          <i class="fa fa-info"></i>
+        </div>
+        <a href="/" class="small-box-footer">
+          <i class="fa fa-home"></i> Home
+        </a>
+      </div>
+    </div>
+    <!-- ./col -->
+  </div>
 </section>
